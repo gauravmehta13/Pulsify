77f3b7a7122f45fbe84602c0cc370d380a9ff8d4
gaurav <270mehta@gmail.com>
2020-09-09 09:31:10 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/gauravmehta13/HRM/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gauravmehta13.HRM
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Heart Rate Monitor',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Homepage(),
+    );
+  }
+}
